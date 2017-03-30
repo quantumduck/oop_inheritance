ccb55d3713d7c091446141f10da4c8adf2024de4
quantumduck <quantum.duck@hotmail.com>
2017-03-30 18:02:41 -0400

    tested a few method calls

--- quote_collector.rb
+++ quote_collector.rb
@@ -3,9 +3,11 @@ require_relative 'multilinguist'
 
 class QuoteCollector < Multilinguist
 
+  # attr_accessor :collection
+
   def initialize
-    @local_lang = 'en'
-    @collection = [{quote: "Hello, World", author: "Anonymous", subject: :general}]
+    super
+    @collection = [{quote: "Hello, World!", author: "Anonymous", subject: :general}]
   end
 
   def collect_quote(quote, author = "Anonymous", subject = :general)
@@ -17,11 +19,17 @@ class QuoteCollector < Multilinguist
     if (@collection[index][:author] == "Anonymous")
       self.say_in_local_language("A wise person once said: \"#{@collection[index][:quote]}\"")
     else
-      self.say_in_local_language("According to #{@collection[index][:author]}: \"#{@collection[index][:quote]}\"")
+      self.say_in_local_language("#{@collection[index][:author]} once said: \"#{@collection[index][:quote]}\"")
     end
   end
 
 end
 
 qq = QuoteCollector.new
-qq.say_random
+puts qq.say_random
+qq.collect_quote("I love Lasagna!", "Garfield", :food)
+puts qq.say_random
+qq.travel_to("India")
+puts qq.say_random
+qq.travel_to("India")
+puts qq.say_random
